3197e3580faaf9db3b8602d907888e6f798fc1c6
cgdjdhahaha <1362117049@qq.com>
2020-09-15 19:13:22 +0800

    何家云项目

---
+++ src/main/java/com/mashibing/FamilyServerPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.mashibing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyServerPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FamilyServerPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mashibing/FamilyServerPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mashibing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyServerPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
